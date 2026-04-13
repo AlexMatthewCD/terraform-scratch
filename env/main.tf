8fd37aff91319b3ec5496e895ae466b286d74521
AlexMatthewCD <alex.matthew@crystaldelta.com>
2026-04-13 17:46:46 +0530

    feat:lock-file-s3-setup
    This was the start of terraform practice project
    - Created the lock file
    - Deployed S3 bucket
    - Using S3 bucket to sync with the lock file for enable seemless collaboration

---
+++ env/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "cd-sandbox"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "${var.app_name}-${var.env_name}-tfstate"
+  tags = {
+    Name        = "${var.app_name}-tfstate"
+    Environment = var.env_name
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "version-tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+terraform {
+  required_version = "~> 1.14.8"
+  backend "s3" {
+    bucket       = "scratch-tf-stg-tfstate"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    profile      = "cd-sandbox"
+    encrypt      = false
+    use_lockfile = true
+  }
+}
---
+++ env/variables.tf
@@ -0,0 +1,8 @@
+variable "app_name" {
+  description = "Name of the application"
+  default     = "scratch-tf"
+}
+variable "env_name" {
+  description = "Name of the environment"
+  default     = "stg"
+}
